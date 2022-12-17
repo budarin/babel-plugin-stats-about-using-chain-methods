5d41566e37bce677b3f7ef939fdd8ac233cde559
Vadim Budarin <budarin-vv@yandex.ru>
2022-12-17 13:50:21 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # babel-plugin-stats-about-using-chain-methods
+
 The plugin collects stats info about using chain methods in the code
+
+## Install
+
+Using npm:
+
+```sh
+npm install --save-dev babel-plugin-stats-about-using-chain-methods
+```
+
+or using yarn:
+
+```sh
+yarn add -D babel-plugin-stats-about-using-chain-methods
+```
+
+## Configure
+
+#### babel.config.js
+
+```js
+const plugin = require('babel-plugin-stats-about-using-chain-methods').default;
+
+module.exports = {
+    plugins: [
+        [
+            plugin,
+            {
+                chainMethods: ['map', 'reduce'], // write a list of methods
+            },
+        ],
+    ],
+};
+```
+
+## Options
+
+### chainMethods
+
+Array of method names to collect usage statistics about.
+By default it is equal to:
+
+```
+
+['trim', 'join', 'filter', 'reduce', 'forEach', 'map']
+
+```
+
+## Analyze code
+
+Run the command
+
+```sh
+babel <path to the module>
+```
+
+The output will contain an object with statistics on the use of call chains from the specified methods in the form of an object
+
+Example output
+
+```sh
+{
+    forEach: 139,
+    join: 122,
+    map: 121,
+    reduce: 44,
+    filter: 43,
+    'filter.map': 7,
+    'filter.join': 4,
+    'filter.reduce': 4,
+    'filter.forEach': 1,
+    'filter.map.join': 1
+    'map.join': 26,
+    'map.filter.join': 2,
+    'map.join.trim': 2,
+    'map.forEach': 1,
+    'map.reduce': 1,
+    'map.map': 1,
+    'join.trim': 3,
+}
+
+```
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import * as t from '@babel/types';
+import { declare } from '@babel/helper-plugin-utils';
+
+let currentPath = '';
+
+interface Options {
+    chainMethods: string[];
+}
+const stats = {} as Record<string, number>;
+
+export default declare((api, { chainMethods = ['trim', 'join', 'filter', 'reduce', 'forEach', 'map'] }: Options) => {
+    return {
+        name: 'stats-plugin',
+
+        visitor: {
+            Program: {
+                exit(): void {
+                    Object.keys(stats).forEach((key) => {
+                        if (key.includes('.')) {
+                            const newKey = key.split('.').reverse().join('.');
+                            const value = stats[key];
+
+                            delete stats[key];
+                            stats[newKey] = value;
+                        }
+                    });
+                    console.log(stats);
+                },
+            },
+
+            CallExpression: {
+                enter(path): void {
+                    if (t.isMemberExpression(path.node.callee) && t.isIdentifier(path.node.callee.property)) {
+                        const methodName = path.node.callee.property.name;
+
+                        if (chainMethods.includes(methodName)) {
+                            if (currentPath === '') {
+                                currentPath = methodName;
+                            } else {
+                                currentPath = currentPath + '.' + methodName;
+                            }
+
+                            if (!stats[currentPath]) {
+                                stats[currentPath] = 1;
+                            } else {
+                                stats[currentPath]++;
+                            }
+
+                            let next = path.node.callee.object;
+
+                            while (
+                                t.isCallExpression(next) &&
+                                t.isMemberExpression(next.callee) &&
+                                t.isIdentifier(next.callee.property) &&
+                                chainMethods.includes(next.callee.property.name)
+                            ) {
+                                currentPath = currentPath + '.' + next.callee.property.name;
+
+                                if (!stats[currentPath]) {
+                                    stats[currentPath] = 1;
+                                } else {
+                                    stats[currentPath]++;
+                                }
+
+                                next = next.callee.object;
+                            }
+
+                            currentPath = '';
+                        }
+                    } else {
+                        currentPath = '';
+                    }
+                },
+            },
+        },
+    };
+});
